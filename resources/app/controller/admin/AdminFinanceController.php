ed5de23b62cb39408ba50c76483c70230ec3153e
Ross Riley <riley.ross@gmail.com>
2013-06-28 12:44:24 +0100

    added filter fields

--- resources/app/controller/admin/AdminFinanceController.php
+++ resources/app/controller/admin/AdminFinanceController.php
@@ -6,4 +6,8 @@ class AdminFinanceController extends AdminComponent{
   public $dashboard = false;
   public $tree_layout = false;
   public $per_page = 60;
+  public $filter_fields=array(
+  	'text' => array('columns'=>array('title', 'payment_type','finance_type'), 'partial'=>'_filters_text', 'fuzzy'=>true),
+     'derivatives' => array('columns'=>array('derivatives'), 'partial'=>'_filters_select', 'opposite_join_column'=>'accessories'),
+  );
 }
